4789376a4b61b380feef5e1b6f25dc90a25cf80e
apudo-Sebby <sebbyapudo@gmail.com>
2022-06-30 20:02:09 +0300

    Finished Bash Introduction on variables and echo command

--- myScript.sh
+++ myScript.sh
@@ -1,4 +1,19 @@
 #! /usr/bin/bash
 
 # ECHO COMMAND
+
 echo hello world!
+
+# VARIABLES
+# Uppercase by Convention
+# Letters, numbers & Underscores
+
+NAME="Sebby"
+echo "My name is $NAME"
+# There is also this syntax that is kinda like a javaScript literal Variable
+echo "My name is ${NAME}"
+
+# USER INPUT
+read -p "Enter your second name: " SECOND_NAME
+echo "The user's second name is ${SECOND_NAME}"
+echo "Hello ${NAME} ${SECOND_NAME}, nice to meet you man!"
