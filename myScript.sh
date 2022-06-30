800becfaf408558380fe8abfc8b331e71790419e
apudo-Sebby <sebbyapudo@gmail.com>
2022-06-30 23:11:09 +0300

    Finished on file conditions

--- myScript.sh
+++ myScript.sh
@@ -46,18 +46,18 @@ NAME="Kanindo"
 # fi
 
 # COMPARISONS
-NUM1=3
-NUM2=5
+# NUM1=3
+# NUM2=5
 
-if [ ${NUM1} -gt ${NUM2} ]
-then
-    echo "${NUM1} is greater than ${NUM2}"
-elif [ ${NUM1} -ge ${NUM2} ]
-then 
-    echo "${NUM1} and ${NUM2} are equal"
-else
-    echo "${NUM2} Is greater than ${NUM1}"
-fi
+# if [ ${NUM1} -gt ${NUM2} ]
+# then
+#     echo "${NUM1} is greater than ${NUM2}"
+# elif [ ${NUM1} -ge ${NUM2} ]
+# then 
+#     echo "${NUM1} and ${NUM2} are equal"
+# else
+#     echo "${NUM2} Is greater than ${NUM1}"
+# fi
 
 #######
 # val1 -eq val2 Returns true if the values are equal
@@ -67,3 +67,26 @@ fi
 # val1 -lt val2 Returns true if the val1 is less than val2
 # val1 -le val2 Returns true if the val1 is less than or equal to val2
 #######
+
+# FILE CONDITIONS
+FILE="test.txt"
+if [ -e "${FILE}" ]
+then 
+    echo "$FILE does exist"
+else
+    echo "$FILE does NOT exist"
+fi
+
+
+########
+# -d file True if the file is a directory
+# -e file True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file True if the provided string is a file
+# -g file True if the group id is set on a file
+# -r file True if the file is readable
+# -s file True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is executable
+
+########
