de672d10000d752cc1b2a0a63a93b7c8dee55901
apudo-Sebby <sebbyapudo@gmail.com>
2022-06-30 20:10:25 +0300

    Finished on the simple IF and IF-ELSE statements

--- myScript.sh
+++ myScript.sh
@@ -2,18 +2,34 @@
 
 # ECHO COMMAND
 
-echo hello world!
+# echo hello world!
 
 # VARIABLES
 # Uppercase by Convention
 # Letters, numbers & Underscores
 
-NAME="Sebby"
-echo "My name is $NAME"
+NAME="Kanindo"
+# echo "My name is $NAME"
 # There is also this syntax that is kinda like a javaScript literal Variable
-echo "My name is ${NAME}"
+# echo "My name is ${NAME}"
 
 # USER INPUT
-read -p "Enter your second name: " SECOND_NAME
-echo "The user's second name is ${SECOND_NAME}"
-echo "Hello ${NAME} ${SECOND_NAME}, nice to meet you man!"
+# read -p "Enter your second name: " SECOND_NAME
+# echo "The user's second name is ${SECOND_NAME}"
+# echo "Hello ${NAME} ${SECOND_NAME}, nice to meet you man!"
+
+# BASH CONDITIONALS
+# SIMPLE IF STATEMENTS
+
+# if [ "${NAME}" == "Sebby" ]
+# then
+#     echo "Your name is Sebby"
+# fi
+
+# IF-ELSE
+if [ "${NAME}" == "Sebby" ]
+then
+    echo "Your name is Sebby"
+else
+    echo "Your name is NOT Sebby"
+fi
