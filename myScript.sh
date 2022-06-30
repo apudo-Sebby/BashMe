211959b791ec300b3f01393d0e7978ddfbb815c7
apudo-Sebby <sebbyapudo@gmail.com>
2022-06-30 21:08:09 +0300

    Finished on comparisons

--- myScript.sh
+++ myScript.sh
@@ -35,12 +35,35 @@ NAME="Kanindo"
 # fi
 
 # ELSE-IF (elif)
-if [ "${NAME}" == "Sebby" ]
+# if [ "${NAME}" == "Sebby" ]
+# then
+#     echo "Your name is Sebby"
+# elif [ "${NAME}" == "Junior" ]
+# then
+#     echo "Your name is neither Junior"
+# else
+#     echo "Your name is NOT Sebby nor Junior"
+# fi
+
+# COMPARISONS
+NUM1=3
+NUM2=5
+
+if [ ${NUM1} -gt ${NUM2} ]
 then
-    echo "Your name is Sebby"
-elif [ "${NAME}" == "Junior" ]
-then
-    echo "Your name is neither Junior"
+    echo "${NUM1} is greater than ${NUM2}"
+elif [ ${NUM1} -ge ${NUM2} ]
+then 
+    echo "${NUM1} and ${NUM2} are equal"
 else
-    echo "Your name is NOT Sebby nor Junior"
+    echo "${NUM2} Is greater than ${NUM1}"
 fi
+
+#######
+# val1 -eq val2 Returns true if the values are equal
+# val1 -ne val2 Returns true if the values are not equal
+# val1 -gt val2 Returns true if the val1 is greater than val2
+# val1 -ge val2 Returns true if the val1 is greater than or equal to val2
+# val1 -lt val2 Returns true if the val1 is less than val2
+# val1 -le val2 Returns true if the val1 is less than or equal to val2
+#######
